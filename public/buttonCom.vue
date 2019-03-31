07ef07495a4925e4b7756c8700a1ff2a334cb9d4
Ramon Cruz <ramon.cruz.comercial@hotmail.com>
2019-03-31 19:39:42 -0300

    formulario

---
+++ public/buttonCom.vue
@@ -0,0 +1,13 @@
+<template>
+   <button v-on:click="count++">Você clicou em mim {{ count }} vezes.</button> 
+</template>
+
+<script>
+  export default {
+    data: function () {
+        return {
+        count: 0
+        }
+    }
+  }
+</script>
